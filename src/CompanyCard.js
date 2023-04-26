69535efd529b71b9f75d781bad727bff1cbfa506
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-26 15:16:22 -0400

    create companyCard

---
+++ src/CompanyCard.js
@@ -0,0 +1,22 @@
+
+/** CompanyCard
+*
+* Props
+* -company {name, description, logoURL]}
+*
+* State none
+*
+* Effect none
+*
+* CompanyList --> CompanyCard
+*/
+
+function CompanyCard({ company }){
+  return(
+    <div className="CompanyCard">
+      <p>I'm a company card</p>
+    </div>
+  );
+}
+
+export default CompanyCard;
