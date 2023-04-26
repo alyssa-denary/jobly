cba5cb4d49c20b38c2f5aeb00eb73793c4267ef4
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-26 14:47:06 -0400

    add SearchForm component

---
+++ src/SearchForm.js
@@ -0,0 +1,23 @@
+
+
+/** SearchForm
+*
+* Props
+* -filter (callback function)
+*
+* State
+* -formData
+*
+* {CompanyList,  JobList} --> SearchForm
+*/
+
+function SearchForm(filter) {
+
+  return (
+    <form className="SearchForm">
+      <p>I'm a Search form</p>
+    </form>
+  );
+}
+
+export default SearchForm;
