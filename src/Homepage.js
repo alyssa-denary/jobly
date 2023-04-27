9e009b9c1e8514275857b12cdd408100a75e6c82
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-27 14:17:32 -0400

    add homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,14 +1,21 @@
+import "./Homepage.css";
+
 /**  Homepage
- * 
+ *
  * Props: none
- * 
+ *
  * State: none
- * 
+ *
  * RouteList --> Homepage
 */
 
 function Homepage() {
-    return null;
+  return (
+    <div className="Homepage d-flex flex-column justify-content-center">
+      <h2>Jobly</h2>
+      <p>All the jobs in one, convenient place.</p>
+    </div>
+  );
 }
 
 export default Homepage;
