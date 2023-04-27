26f48f1aa181335f5427e2096a6144949308627f
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-27 18:58:01 -0400

    add conditional login in routelist

--- src/RouteList.js
+++ src/RouteList.js
@@ -3,45 +3,64 @@ import CompanyList from './CompanyList';
 import JobList from './JobList';
 import CompanyDetail from './CompanyDetail';
 import Homepage from './Homepage';
+import SignUpForm from './SignUpForm';
+import LoginForm from './LoginForm';
 
 /** Route list
  *
  * Props: none
- * 
+ *
  * State : none
  *
  * App --> RouteList --> { Homepage, CompanyList, CompanyDetail, JobList}
  */
 
-function RouteList() {
+function RouteList({ user, loginUser, signUpUser, updateProfile }) {
 
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<Homepage />}
-      />
+      {user.username === null &&
+        <div>
+          <Route
+            path="/"
+            element={<Homepage />}
+          />
 
-      <Route
-        path="/companies"
-        element={<CompanyList />}
-      />
+          <Route
+            path="/login"
+            element={<LoginForm />}
+          />
 
-      <Route
-        path="/companies/:handle"
-        element={<CompanyDetail />}
-      />
+          <Route
+            path="/signup"
+            element={<SignUpForm />}
+          />
+        </div>
+      }
+      {user.username !== null &&
+        <div>
+          < Route
+            path="/companies"
+            element={< CompanyList />}
+          />
 
-      <Route
-        path="/jobs"
-        element={<JobList />}
-      />
+          < Route
+            path="/companies/:handle"
+            element={< CompanyDetail />}
+          />
 
-      <Route
-        path="/*"
-        element={<Navigate to="/" />}
-      />
-    </Routes>
+          < Route
+            path="/jobs"
+            element={< JobList />}
+          />
+
+          < Route
+            path="/*"
+            element={< Navigate to="/" />}
+          />
+        </div>
+      }
+    </Routes >
   );
 }
 
