400add81889e0bfb4d9d5e72642ad5822bd09a49
Kevin Deakers <kmdeakers@mac.com>
2023-04-27 16:49:52 -0700

    Build userContext Finish Homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,8 +1,11 @@
 import "./Homepage.css";
+import { useNavigate }from 'react-router-dom';
+import { useContext } from 'react';
+import userContext from './userContext';
 
 /**  Homepage
  *
- * Props: none
+ * Props: user
  *
  * State: none
  *
@@ -10,10 +13,31 @@ import "./Homepage.css";
 */
 
 function Homepage() {
+  const { user } = useContext(userContext);
+  const navigate = useNavigate();
+
+  function navigateLogIn() {
+    navigate("/login");
+  }
+
+  function navigateSignUp() {
+    navigate("/signup");
+  }
+
   return (
     <div className="Homepage d-flex flex-column justify-content-center">
       <h2>Jobly</h2>
       <p>All the jobs in one, convenient place.</p>
+      {user.username === null &&
+        <div>
+          <button className="btn btn-primary" onClick={navigateLogIn}>Log in</button>
+          <button className="btn btn-primary" onClick={navigateSignUp}>Sign up</button>
+        </div>}
+      {user.username !== null &
+        <div>
+          <h2>Welcome Back, {user.firstName}!</h2>
+        </div>
+      }
     </div>
   );
 }
