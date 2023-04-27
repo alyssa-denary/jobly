0166dad89572d9c9339cc56e9ce9a44c4c2fc890
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-27 18:50:13 -0400

    format doc

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -11,53 +11,53 @@ import { useState } from 'react';
  * RouteList --> LoginForm
  */
 
-function LoginForm ({loginUser}) {
-    const initialData = {username: '', password: ''};
-    const [formData, setFormData] = useState(initialData);
+function LoginForm({ loginUser }) {
+  const initialData = { username: '', password: '' };
+  const [formData, setFormData] = useState(initialData);
 
-    /** Update local state w/curr state of input elem */
+  /** Update local state w/curr state of input elem */
 
-    function handleChange(evt) {
-        const { name, value } = evt.targetj;
-        setFormData(fData => ({
-            ...fData,
-            [name]: value,
-        }));
-    }
+  function handleChange(evt) {
+    const { name, value } = evt.targetj;
+    setFormData(fData => ({
+      ...fData,
+      [name]: value,
+    }));
+  }
 
-    /** Call parent function and clear form */
+  /** Call parent function and clear form */
 
-    function handleSubmit(evt) {
-        evt.preventDefault();
-        loginUser(formData);
-        setFormData(initialData);
-    }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    loginUser(formData);
+    setFormData(initialData);
+  }
 
-    return (
-        <form className='LoginForm' onSubmit={handleSubmit}>
-            <div className='form-group'>
-                <label htmlFor='LoginForm-username'>Username</label>
-                <input
-                    className='form-control'
-                    id='LoginForm-username'
-                    name='username'
-                    onChange={handleChange}
-                    value={formData.username}
-                    />
-            </div>
-            <div className='form-group'>
-                <label htmlFor='LoginForm-password'>Password</label>
-                <input
-                    className='form-control'
-                    id='LoginForm-password'
-                    name='password'
-                    onChange={handleChange}
-                    value={formData.password}
-                    />
-            </div>
-            <button className='btn btn-primary'>Submit</button>
-        </form>
-    );
+  return (
+    <form className='LoginForm' onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <label htmlFor='LoginForm-username'>Username</label>
+        <input
+          className='form-control'
+          id='LoginForm-username'
+          name='username'
+          onChange={handleChange}
+          value={formData.username}
+        />
+      </div>
+      <div className='form-group'>
+        <label htmlFor='LoginForm-password'>Password</label>
+        <input
+          className='form-control'
+          id='LoginForm-password'
+          name='password'
+          onChange={handleChange}
+          value={formData.password}
+        />
+      </div>
+      <button className='btn btn-primary'>Submit</button>
+    </form>
+  );
 
 }
 
--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -11,89 +11,89 @@ import { useState } from 'react';
  * RouteList --> SignUpForm
  */
 
-function SignUpForm ({ signUpUser }) {
-    const initialData = {
-      username: '',
-      password: '',
-      firstName: '',
-      lastName: '',
-      email: ''
-    }
-    const [formData, setFormData] = useState(initialData);
+function SignUpForm({ signUpUser }) {
+  const initialData = {
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: ''
+  };
+  const [formData, setFormData] = useState(initialData);
 
-    /** Update local state w/curr state of input elem */
+  /** Update local state w/curr state of input elem */
 
-    function handleChange(evt) {
-        const { name, value } = evt.targetj;
-        setFormData(fData => ({
-            ...fData,
-            [name]: value,
-        }));
-    }
+  function handleChange(evt) {
+    const { name, value } = evt.targetj;
+    setFormData(fData => ({
+      ...fData,
+      [name]: value,
+    }));
+  }
 
-    /** Call parent function and clear form */
+  /** Call parent function and clear form */
 
-    function handleSubmit(evt) {
-        evt.preventDefault();
-        signUpUser(formData);
-        setFormData(initialData);
-    }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    signUpUser(formData);
+    setFormData(initialData);
+  }
 
-    return (
-        <form className='SignUpForm' onSubmit={handleSubmit}>
-            <div className='form-group'>
-                <label htmlFor='SignUpForm-username'>Username</label>
-                <input
-                    className='form-control'
-                    id='SignUpForm-username'
-                    name='username'
-                    onChange={handleChange}
-                    value={formData.username}
-                    />
-            </div>
-            <div className='form-group'>
-                <label htmlFor='SignUpForm-password'>Password</label>
-                <input
-                    className='form-control'
-                    id='SignUpForm-password'
-                    name='password'
-                    onChange={handleChange}
-                    value={formData.password}
-                    />
-            </div>
-            <div className='form-group'>
-                <label htmlFor='SignUpForm-firstName'>First name</label>
-                <input
-                    className='form-control'
-                    id='SignUpForm-firstName'
-                    name='firstName'
-                    onChange={handleChange}
-                    value={formData.firstName}
-                    />
-            </div>
-            <div className='form-group'>
-                <label htmlFor='SignUpForm-lastName'>Last name</label>
-                <input
-                    className='form-control'
-                    id='SignUpForm-lastName'
-                    name='lastName'
-                    onChange={handleChange}
-                    value={formData.lastName}
-                    />
-            </div>
-            <div className='form-group'>
-                <label htmlFor='SignUpForm-email'>Email</label>
-                <input
-                    className='form-control'
-                    id='SignUpForm-email'
-                    name='email'
-                    onChange={handleChange}
-                    value={formData.email}
-                    />
-            </div>
-            <button className='btn btn-primary'>Submit</button>
-        </form>
-    );
+  return (
+    <form className='SignUpForm' onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <label htmlFor='SignUpForm-username'>Username</label>
+        <input
+          className='form-control'
+          id='SignUpForm-username'
+          name='username'
+          onChange={handleChange}
+          value={formData.username}
+        />
+      </div>
+      <div className='form-group'>
+        <label htmlFor='SignUpForm-password'>Password</label>
+        <input
+          className='form-control'
+          id='SignUpForm-password'
+          name='password'
+          onChange={handleChange}
+          value={formData.password}
+        />
+      </div>
+      <div className='form-group'>
+        <label htmlFor='SignUpForm-firstName'>First name</label>
+        <input
+          className='form-control'
+          id='SignUpForm-firstName'
+          name='firstName'
+          onChange={handleChange}
+          value={formData.firstName}
+        />
+      </div>
+      <div className='form-group'>
+        <label htmlFor='SignUpForm-lastName'>Last name</label>
+        <input
+          className='form-control'
+          id='SignUpForm-lastName'
+          name='lastName'
+          onChange={handleChange}
+          value={formData.lastName}
+        />
+      </div>
+      <div className='form-group'>
+        <label htmlFor='SignUpForm-email'>Email</label>
+        <input
+          className='form-control'
+          id='SignUpForm-email'
+          name='email'
+          onChange={handleChange}
+          value={formData.email}
+        />
+      </div>
+      <button className='btn btn-primary'>Submit</button>
+    </form>
+  );
 
 }
 
