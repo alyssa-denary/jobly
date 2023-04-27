52b4f75e8c68a3af005999a7a948edc4de04a684
Kevin Deakers <kmdeakers@mac.com>
2023-04-26 17:04:04 -0700

    Work on CompanyDetail

--- src/CompanyDetail.js
+++ src/CompanyDetail.js
@@ -1,5 +1,6 @@
 import JobCardList from "./JobCardList";
-
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom"
 /** CompanyDetail
 *
 * Props none
@@ -15,12 +16,37 @@ import JobCardList from "./JobCardList";
 */
 
 function CompanyDetail() {
+    const initialCompanyData = {
+        isLoading: true,
+        data: null,
+        errors: null
+    }
+    const {handle} = useParams();
+    const [company, setCompany] = useState(initialCompanyData);
 
-  const fakeJobs = [{fakejob: null}];
+    useEffect(function fetchCompanyWhenMounted() {
+        async function fetchCompany() {
+            try {
+                const companyResult = await getCompany(handle);
+                setCompany({
+                    isLoading: false,
+                    data: companyResult,
+                    errors: null
+                });
+            } catch(err) {
+                setCompany({
+                    isLoading: false,
+                    data: null,
+                    errors: err 
+                });
+            }
+        }
+    });
+    
 
     return (
         <div className="CompanyDetail">
-            <JobCardList jobs={fakeJobs}/>
+            <JobCardList jobs={}/>
         </div> 
     )
 }
