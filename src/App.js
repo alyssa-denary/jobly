99e7633fb83ae3d0fcea217ae7c3c56c620fa921
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-27 20:21:55 -0400

    add pseudocode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import RouteList from './RouteList';
 import Navigation from './Navigation';
 import { BrowserRouter } from 'react-router-dom';
+import userContext from "./userContext";
+import { useState } from 'react';
 
 /** App
  *
@@ -13,12 +15,37 @@ import { BrowserRouter } from 'react-router-dom';
  */
 
 function App() {
+  const [user, setUser] = useState(null);
+
+
+  function loginUser(){
+    // hold on to username
+    // call api function --> get token
+    // if token then username is valid, call 2nd api function
+    // . . to GET /[username] to get all user info
+    // {username, firstName, lastName, isAdmin, jobs}
+    // setUser with that info
+  }
+
+  function signUpUser(){}
+
+  function profileUpdate(data){
+    // calling api function
+    // setUser with response data
+  }
+
+  function logoutUser(){
+    setUser(null);
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navigation />
-        <RouteList />
-      </BrowserRouter>
+      <userContext.Provider value={user}>
+        <BrowserRouter>
+          <Navigation logout={logoutUser} />
+          <RouteList />
+        </BrowserRouter>
+      </userContext.Provider>
     </div>
   );
 }
