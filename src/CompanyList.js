03ed88f3508e27ae78d7accd953ac861ebf313f8
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-28 08:15:37 -0400

    add bootstrap classes to companyList

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -65,7 +65,7 @@ function CompanyList() {
   }
 
   return (
-    <div className="CompanyList">
+    <div className="CompanyList col-md-8 offset-md-2">
       <SearchForm filter={filter} />
       {companies.isLoading && <p>Loading...</p>}
       {/* TODO: Add empty array to not found conditional */}
